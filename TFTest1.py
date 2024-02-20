de7b8399a62b67d4e7d0df165cf09dc067138123
hongw <hongw@mun.ca>
2024-02-19 20:07:35 -0600

    add comment

--- TFTest1.py
+++ TFTest1.py
@@ -50,4 +50,6 @@ def run_training(epochs=1):
 
 
 # Running the first time creates the model
+print(tf.config.list_physical_devices('CPU'))
+print(tf.config.list_physical_devices('GPU'))
 run_training(2)
