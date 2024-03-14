03ccae8ef2438074834d06ba5830a05a58b48254
hola <hongw@mun.ca>
2024-03-14 20:16:48 -0230

    update load py version

--- pytorch-ddp-test.sh
+++ pytorch-ddp-test.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=%N-%j.out
 
 #module load python # Using Default Python version - Make sure to choose a version that suits your application
-module load python
+module load python/3.10
 srun --tasks-per-node=2 bash << EOF
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
