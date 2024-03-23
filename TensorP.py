3bfaf565b63e39ef8c5f7be3daa0356beaaa6574
root <hongw@mun.ca>
2024-03-23 19:16:31 -0230

    add tensorP

---
+++ TensorP.py
@@ -0,0 +1,6 @@
+from torch.distributed.tensor.parallel import parallelize_module, ColwiseParallel, RowwiseParallel
+from torch.distributed.device_mesh import init_device_mesh
+# Define the module.
+m = Model(...)
+tp_mesh = init_device_mesh("cuda", (8,))
+m = parallelize_module(m, tp_mesh, {"w1": ColwiseParallel(), "w2": RowwiseParallel()})
