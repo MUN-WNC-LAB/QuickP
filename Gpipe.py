39ec012f646c5d2a8d87a7406bd3d9afa1b9bf05
hongw <hongw@mun.ca>
2024-03-24 16:29:16 -0230

    refactor

--- Gpipe.py
+++ Gpipe.py
@@ -6,6 +6,8 @@ import time
 import torchvision
 from torchvision import transforms
 
+from PyUtil import getStdModelForCifar10, getStdCifar10DataLoader
+
 # This guide can only be run with the torch backend. must write when using both keras and pytorch
 # sudo apt install python3-packaging
 os.environ["KERAS_BACKEND"] = "torch"
@@ -35,20 +37,7 @@ args = parser.parse_args()
 # Update world size
 args.world_size = args.gpus * args.num_workers
 
-# our module must be nn.Sequential as GPipe will automatically split the module into partitions with consecutive layers
-# The previous layer's out_channels should match the next layer's in_channels
-# https://stackoverflow.com/questions/68606661/what-is-difference-between-nn-module-and-nn-sequential;
-# using nn-module or nn-sequential
-model = nn.Sequential(
-    nn.Conv2d(3, 6, 5),
-    nn.MaxPool2d(2, 2),
-    nn.Conv2d(6, 16, 5),
-    nn.MaxPool2d(2, 2),
-    nn.Flatten(),
-    nn.Linear(16 * 5 * 5, 120),
-    nn.Linear(120, 84),
-    # the out_features number of the last layer should match the class number
-    nn.Linear(84, 10))
+model = getStdModelForCifar10()
 model.cuda()
 
 # balance's length is equal to the number of computing nodes
@@ -72,13 +61,7 @@ batch_size = 64
 If Use keras dataset instead of torchvision 
 https://keras.io/guides/writing_a_custom_training_loop_in_torch/ 
 '''
-# Data loading code for CiFar10
-transform_train = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_train, download=True)
-# sampler=train_sampler; if sampler is defined, set the shuffle to false
-train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,
-                                               pin_memory=True, num_workers=1)
+train_dataloader = getStdCifar10DataLoader(batch_size, 1)
 # start training
 epochs = 3
 epoch_start = time.time()
---
+++ PyUtil.py
@@ -0,0 +1,44 @@
+import os
+
+import torchvision
+from torchvision import transforms
+
+# This guide can only be run with the torch backend. must write when using both keras and pytorch
+# sudo apt install python3-packaging
+os.environ["KERAS_BACKEND"] = "torch"
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+
+# our module must be nn.Sequential as GPipe will automatically split the module into partitions with consecutive layers
+# The previous layer's out_channels should match the next layer's in_channels
+# https://stackoverflow.com/questions/68606661/what-is-difference-between-nn-module-and-nn-sequential;
+# using nn-module or nn-sequential
+def getStdModelForCifar10():
+    return nn.Sequential(
+        nn.Conv2d(3, 6, 5),
+        nn.MaxPool2d(2, 2),
+        nn.Conv2d(6, 16, 5),
+        nn.MaxPool2d(2, 2),
+        nn.Flatten(),
+        nn.Linear(16 * 5 * 5, 120),
+        nn.Linear(120, 84),
+        # the out_features number of the last layer should match the class number
+        nn.Linear(84, 10))
+
+
+# Data loading code for CiFar10
+def getStdCifar10DataLoader(batch_size, num_workers=1):
+    """
+    If Use keras dataset instead of torchvision
+    https://keras.io/guides/writing_a_custom_training_loop_in_torch/
+    """
+    # Data loading code for CiFar10
+    transform_train = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_train, download=True)
+    # sampler=train_sampler; if sampler is defined, set the shuffle to false
+    return torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,
+                                       pin_memory=True, num_workers=num_workers)
