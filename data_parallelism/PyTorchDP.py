c86166870224c9eca70798daa5b0143e306cd91a
hola <hongw@mun.ca>
2024-03-27 18:10:32 -0230

    upfate timing var

--- data_parallelism/PyTorchDP.py
+++ data_parallelism/PyTorchDP.py
@@ -90,7 +90,7 @@ def train(epoch, net, criterion, optimizer, train_loader, train_rank):
 
         elapse_time = datetime.datetime.now().timestamp() - epoch_start.timestamp()
         elapse_time = datetime.timedelta(seconds=elapse_time)
-        if batch_idx % 15 == 0:
+        if batch_idx % 30 == 0:
             print("From Rank: {}, Training time {}, epoch {}, steps {}".format(train_rank, elapse_time, epoch, batch_idx))
     if epoch == (args.max_epochs - 1):
         ending_time = datetime.datetime.now()
