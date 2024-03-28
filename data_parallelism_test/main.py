92736904b18a8b4f3b0d9666d4483428ef0798d7
hola <hongw@mun.ca>
2024-03-27 22:01:05 -0230

    update all

--- data_parallelism_test/main.py
+++ data_parallelism_test/main.py
@@ -14,57 +14,13 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 
 sys.path.append("../")
-from PyUtil import getStdModelForCifar10
+from PyUtil import getStdModelForCifar10, getArgs
 
 
 # https://gist.github.com/TengdaHan/1dd10d335c7ca6f13810fff41e809904
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--net', default='resnet18', type=str)
-    parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
-    parser.add_argument('--batch_size', default=256, type=int, help='batch size per GPU')
-    parser.add_argument('--gpu', default=None, type=int)
-    parser.add_argument('--start_epoch', default=0, type=int,
-                        help='start epoch number (useful on restarts)')
-    parser.add_argument('--epochs', default=2, type=int, help='number of total epochs to run')
-    # DDP configs:
-    parser.add_argument('--num_workers', type=int, default=2, help='')
-    parser.add_argument('--world-size', default=-1, type=int,
-                        help='number of nodes for distributed training')
-    parser.add_argument('--rank', default=-1, type=int,
-                        help='node rank for distributed training')
-    parser.add_argument('--dist-url', default='tcp://192.168.0.66:3456', type=str,
-                        help='url used to set up distributed training')
-    parser.add_argument('--dist-backend', default='nccl', type=str,
-                        help='distributed backend')
-    parser.add_argument('--local_rank', default=-1, type=int,
-                        help='local rank for distributed training')
-    args = parser.parse_args()
-    return args
-
 
 def main(args):
     nodeID = int(os.environ.get("SLURM_NODEID"))
-    print("NodeID: ", nodeID)
-    # DDP setting
-    # update world size, rank, and if distributed in the args
-    if "WORLD_SIZE" in os.environ:
-        args.world_size = int(os.environ["WORLD_SIZE"])
-    args.distributed = args.world_size > 1
-    ngpus_per_node = torch.cuda.device_count()
-
-    if args.distributed:
-        if args.local_rank != -1:  # for torch.distributed.launch
-            args.rank = args.local_rank
-            args.gpu = args.local_rank
-        elif 'SLURM_PROCID' in os.environ:  # for slurm scheduler
-            args.rank = int(os.environ['SLURM_PROCID'])
-            args.gpu = args.rank % torch.cuda.device_count()
-        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-                                world_size=args.world_size, rank=args.rank)
-
-    print("Rank", args.rank, "World size: ", args.world_size)
-
     ### model ###
     model = getStdModelForCifar10()
     if args.distributed:
@@ -161,5 +117,5 @@ def validate(val_loader, model, criterion, epoch, args):
 '''
 
 if __name__ == '__main__':
-    args = parse_args()
+    args = getArgs()
     main(args)
