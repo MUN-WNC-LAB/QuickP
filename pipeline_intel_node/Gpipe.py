f1f5185596a84aebdd91584fcbf8f2deaae81837
hola <hongw@mun.ca>
2024-03-28 16:50:11 -0230

    update log 3

--- pipeline_intel_node/Gpipe.py
+++ pipeline_intel_node/Gpipe.py
@@ -135,14 +135,14 @@ if args.rank == 0:
     schedule.step(x)
     ending_time = datetime.datetime.now()
     print("Rank",  args.rank, " Beginning time ", beginning_time, " Ending time ", ending_time,
-          " Elapsed time ", ending_time.timestamp() - beginning_time.timestamp())
+          " Elapsed time ", datetime.timedelta(seconds=ending_time.timestamp() - beginning_time.timestamp()))
 # the last node
 elif args.rank == args.world_size - 1:
     beginning_time = datetime.datetime.now()
     output = schedule.step()
     ending_time = datetime.datetime.now()
     print("Rank", args.rank, " Beginning time ", beginning_time, " Ending time ", ending_time,
-          " Elapsed time ", ending_time.timestamp() - beginning_time.timestamp())
+          " Elapsed time ", datetime.timedelta(seconds=ending_time.timestamp() - beginning_time.timestamp()))
 # intermediate nodes
 else:
     schedule.step()
