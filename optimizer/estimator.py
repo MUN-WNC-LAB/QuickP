0ea761bb4c666abaa7c8106e30885c57aed5b8ea
hola <hongw@mun.ca>
2024-04-24 16:33:38 -0230

    can run now

--- optimizer/estimator.py
+++ optimizer/estimator.py
@@ -29,25 +29,20 @@ model.setParam("IntFeasTol", 1e-6)
 # Define variables
 x = {}  # key will be (node_id, machine_id), value will be 1 or 0
 d = {}  # key will be (node_id_1, node_id_2), value will be 1 or 0
-for node_id in list(comp_graph.getOperatorIDs()):
-    for machine_id in list(deviceTopo.getDeviceIDs()):
+for node_id in comp_graph.getOperatorIDs():
+    for machine_id in deviceTopo.getDeviceIDs():
         x[node_id, machine_id] = model.addVar(vtype=GRB.BINARY)
-for edge_id_tuple in list(comp_graph.getEdgeIDs()):
+for edge_id_tuple in comp_graph.getEdgeIDs():
     d[edge_id_tuple[0], edge_id_tuple[1]] = model.addVar(vtype=GRB.BINARY)
 # for
 
 # Add constraints that schedule every node on exactly one machine
-node_schedule_count = {}
-# map the node_id to the times it is assigned
-for key, value in x.items():
-    # (node_id, machine_id) is the key and key[0] is the node_id. Time complexity is only n
-    node_id = key[0]
-    if node_id not in node_schedule_count:
-        node_schedule_count[node_id] = model.addVar(vtype=GRB.INTEGER, lb=0)
-    node_schedule_count[node_id] += value
-for times in list(node_schedule_count.values()):
-    model.addConstr(times == 1, "one op can only be scheduled once")
-
+for node_id in comp_graph.getOperatorIDs():
+    times_scheduled = LinExpr()
+    for machine_id in deviceTopo.getDeviceIDs():
+        times_scheduled += x[node_id, machine_id]
+    model.addConstr(times_scheduled == 1,"every node on exactly one machine")
+'''
 # Add constraints that operators assigned cannot exceed the capacity
 device_mem_count = {}
 # map the node_id to the times it is assigned
@@ -62,17 +57,13 @@ for key, value in x.items():
 for key, value in device_mem_count.items():
     device_capacity = deviceTopo.getDevice(key)["memory_capacity"]
     model.addConstr(value <= device_capacity, "satisfy each deice's memory constraint")
-
+'''
 # Add constraints that each device should have at least one operator assigned
-device_op_count = {}
-for key, value in x.items():
-    # (node_id, machine_id) is the key and key[1] is the machine_id. Time complexity is only n
-    device_id = key[1]
-    if device_id not in device_op_count:
-        device_op_count[device_id] = model.addVar(vtype=GRB.INTEGER, lb=0)
-    device_op_count[device_id] += value
-for number_op in list(device_op_count.values()):
-    model.addConstr(number_op >= 1, "each device should have at least one op")
+for machine_id in deviceTopo.getDeviceIDs():
+    op_count = LinExpr()
+    for node_id in comp_graph.getOperatorIDs():
+        op_count += x[node_id, machine_id]
+    model.addConstr(op_count >= 1, "each device should have at least one op")
 
 # Add constraints that later operator cannot begin before all previous ones finish computing and transmission
 start = {}
