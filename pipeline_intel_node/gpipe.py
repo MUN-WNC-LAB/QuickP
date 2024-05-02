8101c30bbaa50e0476c0b226c5bf369429647571
hola <hongw@mun.ca>
2024-05-02 18:26:02 -0230

    update gpipe prof 4

--- pipeline_intel_node/gpipe.py
+++ pipeline_intel_node/gpipe.py
@@ -77,11 +77,13 @@ with profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
 ) as prof:
     for epoch in range(args.epochs):  # change to no. epochs
+        '''
         save_checkpoint(
             pipe,
             checkpoint_dir=os.path.join("checkpoints", f"{epoch + 1}"),
             optimizer=optimizer,
         )
+        '''
         for batch_idx, (inputs, targets) in enumerate(dataLoader, 0):
             x = inputs.to(device)
             y = targets.to(device)
