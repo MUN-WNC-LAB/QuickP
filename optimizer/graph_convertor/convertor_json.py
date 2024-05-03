f98d35cdbea14104d9abc8f1894b5082040cf9d8
hola <hongw@mun.ca>
2024-05-03 15:16:49 -0230

    add new profiler way

--- optimizer/graph_convertor/convertor_json.py
+++ optimizer/graph_convertor/convertor_json.py
@@ -14,7 +14,7 @@ torch.onnx.export(model,                                # model being run
 '''
 
 # network
-net = getStdModelForCifar10()
+net = vgg11()
 
 # Input to the model
 transform_train = transforms.Compose(
@@ -29,7 +29,7 @@ torch.onnx.export(net,  # model being run
                   x,  # model input (or a tuple for multiple inputs)
                   "example.onnx",  # where to save the model (can be a file or file-like object)
                   export_params=True,  # store the trained parameter weights inside the model file
-                  opset_version=9,  # the ONNX version to export the model to
+                  opset_version=17,  # the ONNX version to export the model to
                   do_constant_folding=True,  # whether to execute constant folding for optimization
                   input_names=['X'],  # the model's input names
                   output_names=['Y']  # the model's output names
---
+++ optimizer/profiler/pro_jit.py
@@ -0,0 +1,34 @@
+import torch
+import torchvision
+from torch._C._profiler import ProfilerActivity
+from torch.autograd.profiler import record_function
+from torch.profiler import profile
+from torchvision import transforms
+
+from vgg import vgg11
+
+model = vgg11().cuda()
+example_input = torch.rand(1, 3, 32, 32).cuda()  # Adjust size according to the model
+traced_model = torch.jit.trace(model, example_input)
+
+nodes = {}
+edges = []
+
+for node in traced_model.graph.nodes():
+    if node.kind() != 'prim::Constant':
+        node_id = node.output().debugName()  # Unique identifier for the node
+        nodes[node_id] = {
+            "name": node.scopeName(),  # Human-readable name if available, or uses scope name
+            "id": node_id,
+            "type": node.kind(),
+            "inputs": [inp.debugName() for inp in node.inputs()],
+            "outputs": [out.debugName() for out in node.outputs()]
+        }
+        for inp in node.inputs():
+            if inp.node().kind() != 'prim::Constant':
+                edges.append({"from": inp.debugName(), "to": node_id})
+
+graph_dict = {"nodes": nodes, "edges": edges}
+print(graph_dict)
+for i, data in enumerate(traced_model.graph.nodes()):
+    print(i, data)
