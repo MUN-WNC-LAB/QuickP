b4ec126a61817589a171e50682c0ce45f385d988
hola <hongw@mun.ca>
2024-05-04 16:06:17 -0230

    unable to use ort runtime

--- optimizer/graph_convertor/workflow.py
+++ optimizer/graph_convertor/workflow.py
@@ -1,3 +1,5 @@
+import numpy as np
+import onnxruntime as ort
 import torch
 import torchvision
 from torchvision import transforms
@@ -32,4 +34,15 @@ model_to_onnx(net, x, path)
 graph_dict = onnx_to_dict(path)
 to_json(graph_dict, "onnx_graph.json")
 
-session = ort.InferenceSession(model_path, providers=['CPUExecutionProvider'])
+sess_options = ort.SessionOptions()
+sess_options.enable_profiling = True
+print(ort.get_available_providers())
+sess_profile = ort.InferenceSession("example.onnx", sess_options=sess_options,
+                                    providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+
+input_name = sess_profile.get_inputs()[0].name
+# put x from GPU to CPU
+x = x.cpu().numpy()
+sess_profile.run(None, {input_name: x})
+profile_file = sess_profile.end_profiling()
+print(profile_file)
