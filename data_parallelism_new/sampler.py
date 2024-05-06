bc1160db5230c36657a82e41fcea79053b6b3352
hola <hongw@mun.ca>
2024-05-06 18:34:31 -0230

    fix import 5

--- data_parallelism_new/sampler.py
+++ data_parallelism_new/sampler.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import DistributedSampler
 
 
@@ -17,6 +18,7 @@ class UnevenDistributedSampler(DistributedSampler):
         """
         # indices is a list of element index of a dataset
         """
+        '''
         indices = list(range(len(self.dataset)))  # type: ignore[arg-type]
 
         start = sum(self.split_ratio_list[:self.rank]) * len(self.dataset)  # type: ignore[arg-type]
@@ -24,4 +26,30 @@ class UnevenDistributedSampler(DistributedSampler):
         length = len(self.dataset) * ratio  # type: ignore[arg-type]
         indices = indices[int(start): int(start+length)]
         assert len(indices) == length
+        return iter(indices)
+        '''
+        if self.shuffle:
+            # deterministically shuffle based on epoch and seed
+            g = torch.Generator()
+            g.manual_seed(self.seed + self.epoch)
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()  # type: ignore[arg-type]
+        else:
+            indices = list(range(len(self.dataset)))  # type: ignore[arg-type]
+
+        if not self.drop_last:
+            # add extra samples to make it evenly divisible
+            padding_size = self.total_size - len(indices)
+            if padding_size <= len(indices):
+                indices += indices[:padding_size]
+            else:
+                indices += (indices * math.ceil(padding_size / len(indices)))[:padding_size]
+        else:
+            # remove tail of data to make it evenly divisible.
+            indices = indices[:self.total_size]
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
+
         return iter(indices)
