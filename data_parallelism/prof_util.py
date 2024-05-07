4bcc95772b5a81ee9dc3bd115fa36c92ca787566
hola <hongw@mun.ca>
2024-05-07 19:14:36 -0230

    add filter 3

--- data_parallelism/prof_util.py
+++ data_parallelism/prof_util.py
@@ -3,10 +3,10 @@ def print_communication_cost(table_str):
     lines = table_str.split('\n')
 
     # Initialize a list to hold the titles
-    filtered_lines = lines[0:4]
+    filtered_lines = lines[0:3].append(lines[-2:0])
 
     # Search for rows that contain the keyword 'AllReduce'
-    for line in lines[4:]:
+    for line in lines[3:]:
         if 'all_reduce' in line:
             filtered_lines.append(line)
 
