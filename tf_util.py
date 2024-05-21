84d8eea519ce9cd7cbe9678dc3047eceabcf919a
hola <hongw@mun.ca>
2024-05-21 15:57:42 -0230

    int mem

--- tf_util.py
+++ tf_util.py
@@ -195,7 +195,7 @@ def update_graph_with_prof(graph, prof_dict, mem_dict):
 
         # Update computation cost if available
         if node_id in prof_dict:
-            operator_dict["comp_cost"][device_name] = prof_dict[node_id]
+            operator_dict["comp_cost"][device_name] = int(prof_dict[node_id])
         else:
             operator_dict["comp_cost"][device_name] = 0
         # Update memory cost if available
