9d99450f057b349a294e0978eabe0de2877d0f3f
hola <hongw@mun.ca>
2024-06-07 21:39:09 -0230

    try 5

--- optimizer/device_topo/all_intra_node_topo_parallel.py
+++ optimizer/device_topo/all_intra_node_topo_parallel.py
@@ -13,32 +13,10 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-# Function to get a key that includes a specific substring
-def get_key_including_substring(d, substring):
-    for key in d:
-        if substring in key:
-            return key
-    return None  # Return None if no such key is found
-
-
 def get_intra_node_topo() -> DeviceGraph:
     G = DeviceGraph()
     bandwidths, devices = get_device_bandwidth()
-    for (name, attributes) in devices.items():
-        G.add_new_node(name, attributes["memory_limit"])
-    for (direction, band) in bandwidths.items():
-        if direction == "H2D":
-            from_device = get_key_including_substring(G.nodes, "CPU:0")
-            to_device = get_key_including_substring(G.nodes, "GPU:0")
-        elif direction == "D2H":
-            from_device = get_key_including_substring(G.nodes, "GPU:0")
-            to_device = get_key_including_substring(G.nodes, "CPU:0")
-        else:
-            continue
-        if not from_device or not to_device:
-            raise ValueError("device not found")
-        G.update_link_bandwidth(from_device, to_device, band)
-    print("INFO ROW: ", os.environ.get("SLURMD_NODENAME"), G.edges.data())
+    print("bandwidths: ", G.edges.data(), "devices: ", devices)
     return G
 
 
--- optimizer/device_topo/workflow_device_topo.py
+++ optimizer/device_topo/workflow_device_topo.py
@@ -1,5 +1,10 @@
 import subprocess
 
+from networkx import DiGraph
+
+from optimizer.device_topo.intra_node_bandwidth import get_device_bandwidth
+from optimizer.model.graph import DeviceGraph
+
 output_path = "device_intra_node_output.txt"
 sh_path = "all_device_intra.sh"
 
@@ -54,6 +59,33 @@ def retrieve_slurm_output():
 
     for line in lines:
         print(line.strip())  # strip() to remove leading/trailing whitespace including newline characters
+
+
+def phase_slurm_2_DiGraph() -> DiGraph:
+    # Function to get a key that includes a specific substring
+    def get_key_including_substring(d, substring):
+        for key in d:
+            if substring in key:
+                return key
+        return None  # Return None if no such key is found
+    G = DeviceGraph()
+    bandwidths, devices = get_device_bandwidth()
+    for (name, attributes) in devices.items():
+        G.add_new_node(name, attributes["memory_limit"])
+    for (direction, band) in bandwidths.items():
+        if direction == "H2D":
+            from_device = get_key_including_substring(G.nodes, "CPU:0")
+            to_device = get_key_including_substring(G.nodes, "GPU:0")
+        elif direction == "D2H":
+            from_device = get_key_including_substring(G.nodes, "GPU:0")
+            to_device = get_key_including_substring(G.nodes, "CPU:0")
+        else:
+            continue
+        if not from_device or not to_device:
+            raise ValueError("device not found")
+        G.update_link_bandwidth(from_device, to_device, band)
+    print("INFO ROW: ", G.edges.data())
+    return G
 
 
 if __name__ == "__main__":
