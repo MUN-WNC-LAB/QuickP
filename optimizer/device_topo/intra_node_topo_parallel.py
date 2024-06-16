79f3d8bfe3b1858d29c2509f310d12eb795abd28
hola <hongw@mun.ca>
2024-06-16 18:06:39 -0230

    change path sys 5

--- optimizer/device_topo/intra_node_topo_parallel.py
+++ optimizer/device_topo/intra_node_topo_parallel.py
@@ -4,12 +4,15 @@ This file is called by a Slurm script .sh file
 import os
 import sys
 
-from optimizer.device_topo.intra_node_util import get_device_bandwidth
-from optimizer.model.graph import DeviceGraph
-
 import warnings
 
 warnings.filterwarnings("ignore")
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(script_dir, '..', '..'))
+sys.path.append(project_root)
+from optimizer.device_topo.intra_node_util import get_device_bandwidth
+from optimizer.model.graph import DeviceGraph
 
 
 def get_intra_node_topo() -> DeviceGraph:
