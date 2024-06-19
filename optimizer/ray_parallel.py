0a43dc6d82312c49debec84eaffbc57791881a73
hola <hongw@mun.ca>
2024-06-19 18:11:11 -0230

    ray able to run single node

--- optimizer/ray_parallel.py
+++ optimizer/ray_parallel.py
@@ -1,5 +1,6 @@
 # ray_parallel.py
-
+import torch
+import tensorflow as tf
 import os
 import sys
 import warnings
@@ -10,18 +11,23 @@ import shutil
 warnings.filterwarnings("ignore")
 
 
+# ray stop
+# ray start --head --node-ip-address=192.168.0.66 --port=6379 --dashboard-port=8265 --num-cpus=4 --num-gpus=1
+# ray start --address='192.168.0.66:6379'
+# ray job submit --working-dir /home/hola/Desktop/DNN -- python3 ray_parallel.py
 @ray.remote
 def remote_setup_and_run():
+    # The following two lines must be present
+    import torch
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
     script_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.abspath(os.path.join(script_dir, '..'))
     sys.path.append(project_root)
 
     # Import the function
-    try:
-        from optimizer.device_topo.intra_node_topo_parallel import get_intra_node_topo
-    except ImportError as e:
-        print(f"Import error: {e}", file=sys.stderr)
-        return None
+    from optimizer.device_topo.intra_node_topo_parallel import get_intra_node_topo
+    return get_intra_node_topo()
 
 
 if __name__ == "__main__":
@@ -30,4 +36,3 @@ if __name__ == "__main__":
 
     # Run the function in parallel on the Ray cluster
     print(ray.get(remote_setup_and_run.remote()))
-
