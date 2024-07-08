455bc96577fcc6cd6376e8e93bd6528e9c9f38cc
Hong Wang <hongw@mun.ca>
2024-07-08 01:42:54 -0230

    update get_computation_graph

--- optimizer/computing_graph/computing_graph.py
+++ optimizer/computing_graph/computing_graph.py
@@ -42,14 +42,24 @@ def get_computation_graph(model: tf.keras.Model, optimizer=keras.optimizers.Adam
         train_accuracy.update_state(train_y, predictions)
         return loss
 
-    # tf.TensorSpec constrain the type of inputs accepted by a tf.function
-    # shape=[200, 32, 32, 3], 200 is batch size, 32x32x3 is the size for each image
-    inputs_constraint = tf.TensorSpec(shape=[batch_size, 32, 32, 3], dtype=tf.float32, name="input")
-    targets_constraint = tf.TensorSpec(shape=[batch_size, 1], dtype=tf.uint8, name="target")
-    # to obtain a concrete function from a tf.function.
-    # ConcreteFunctions can be executed just like PolymorphicFunctions,
-    # but their input is restricted to the types to which they're specialized.
-    concrete_function = training_step.get_concrete_function(inputs_constraint, targets_constraint)
+    if isinstance(model, Sequential):
+        # tf.TensorSpec constrain the type of inputs accepted by a tf.function
+        # shape=[200, 32, 32, 3], 200 is batch size, 32x32x3 is the size for each image
+        inputs_constraint = tf.TensorSpec(shape=[batch_size, 32, 32, 3], dtype=tf.float32, name="input")
+        targets_constraint = tf.TensorSpec(shape=[batch_size, 1], dtype=tf.uint8, name="target")
+        # to obtain a concrete function from a tf.function.
+        # ConcreteFunctions can be executed just like PolymorphicFunctions,
+        # but their input is restricted to the types to which they're specialized.
+        concrete_function = training_step.get_concrete_function(inputs_constraint, targets_constraint)
+    elif isinstance(model, TFGPT2LMHeadModel):
+        input_ids_constraint = tf.TensorSpec(shape=[batch_size, 128], dtype=tf.int32, name="input_ids")
+        attention_mask_constraint = tf.TensorSpec(shape=[batch_size, 128], dtype=tf.int32, name="attention_mask")
+        labels_constraint = tf.TensorSpec(shape=[batch_size, 128], dtype=tf.int32, name="labels")
+        concrete_function = training_step.get_concrete_function(input_ids_constraint, labels_constraint,
+                                                                attention_mask_constraint)
+    else:
+        raise ValueError("Wrong model type")
+
     graph = parse_to_comp_graph(concrete_function)
     parent_directory = profile_train(concrete_function, get_cifar_data_loader(batch_size, True), num_prof_step=20)
     plane_pb_file = find_specific_pb_file(parent_directory, "xplane.pb")
