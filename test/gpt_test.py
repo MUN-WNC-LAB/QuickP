f02e023984e101334a79b7271cb9d1296a272d90
hongw <hongw@mun.ca>
2024-07-09 14:08:44 -0230

    hopefully

--- test/gpt_test.py
+++ test/gpt_test.py
@@ -12,12 +12,14 @@ from optimizer.computing_graph.tool import Conf_TB, CONF
 from optimizer.model.graph import CompGraph
 from optimizer.computing_graph.op_graph_util import compile_model, train_loss, train_accuracy, parse_to_comp_graph, \
     process_op_df, update_graph_with_prof, profile_train, get_cifar_data_loader, parse_tensorboard, \
-    find_specific_pb_file, process_mem_dict, get_gpt_data_loader
+    find_specific_pb_file, process_mem_dict, get_gpt_data_loader, get_proper_optimizer
 
 print("TensorFlow version:", tf.__version__)
 print("Keras version:", keras.__version__)
 
 model, tokenizer = get_openai_gpt2_model_and_tokenizer()
+
+optimizer = get_proper_optimizer(model)
 
 
 def get_computation_graph(model: keras.Model, optimizer=keras.optimizers.Adam(3e-4),
@@ -26,4 +28,4 @@ def get_computation_graph(model: keras.Model, optimizer=keras.optimizers.Adam(3e
     compile_model(model, optimizer, loss_fn)
 
 
-get_computation_graph(model, tokenizer=tokenizer)
+get_computation_graph(model, optimizer=optimizer, tokenizer=tokenizer)
