a666448c316938888c8364103aa54324b51027c5
hola <hongw@mun.ca>
2024-07-28 17:02:06 -0230

    add dic

--- optimizer/experiment_figure_generation/auto_run_optimization.py
+++ optimizer/experiment_figure_generation/auto_run_optimization.py
@@ -9,8 +9,19 @@ from optimizer.experiment_figure_generation.tf_model_enum import TFModelEnum
 from optimizer.graph_partitioner.weight_functions import EdgeWeightFunction
 
 
-def populate_parameter_list():
-    pass
+def populate_parameter_list(increment=0.05):
+    ratio_list = [round(x, 10) for x in [0 + i * increment for i in range(int(1 / increment) + 1)]]
+    data_dict = {
+        "models": ratio_list,
+        "no_adjustment": [],
+        "edge_adjustment": [],
+        "node_adjustment": [],
+        "both_adjustment": []
+    }
+    for ratio in ratio_list:
+        pass
+
+    return data_dict
 
 
 def run_optimization_command(problem_type: OptimizationProblem,
@@ -22,39 +33,40 @@ def run_optimization_command(problem_type: OptimizationProblem,
         problem_type(number_of_devices=number_of_devices, model_type=model_type)
     elif problem_type == OptimizationProblem.GRAPH_PARTITION:
         # Call optimize_after_graph_partition
-        problem_type(number_of_devices=number_of_devices, model_type=model_type, edge_weight_function=edge_weight_function)
+        problem_type(number_of_devices=number_of_devices, model_type=model_type,
+                     edge_weight_function=edge_weight_function)
     else:
         raise ValueError("Invalid optimization problem type")
 
 
-def generate_graph():
+def generate_graph(data_dict):
     import matplotlib.pyplot as plt
     import numpy as np
 
     # Sample data
-    models = ['Model 1', 'Model 2', 'Model 3']
+    fix_rate_ratio = [0.1, 0.2, 0.3]
     training_times_no_adjustment = [10, 20, 30]
     training_times_edge_adjustment = [15, 25, 35]
     training_times_node_adjustment = [12, 22, 32]
     training_times_both_adjustment = [14, 24, 34]
 
-    x = np.arange(len(models))  # the label locations
+    x = np.arange(len(data_dict["ratios"]))  # the label locations
     width = 0.2  # the width of the bars
 
     fig, ax = plt.subplots(figsize=(10, 6))
 
     # Plotting the bars
-    bars1 = ax.bar(x - 1.5 * width, training_times_no_adjustment, width, label='No Adjustment')
-    bars2 = ax.bar(x - 0.5 * width, training_times_edge_adjustment, width, label='Edge Weight Adjustment')
-    bars3 = ax.bar(x + 0.5 * width, training_times_node_adjustment, width, label='Node Weight Adjustment')
-    bars4 = ax.bar(x + 1.5 * width, training_times_both_adjustment, width, label='Both Adjustments')
+    bars1 = ax.bar(x - 1.5 * width, data_dict["no_adjustment"], width, label='No Adjustment')
+    bars2 = ax.bar(x - 0.5 * width, data_dict["edge_adjustment"], width, label='Edge Weight Adjustment')
+    bars3 = ax.bar(x + 0.5 * width, data_dict["node_adjustment"], width, label='Node Weight Adjustment')
+    bars4 = ax.bar(x + 1.5 * width, data_dict["both_adjustment"], width, label='Both Adjustments')
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_xlabel('Models')
     ax.set_ylabel('Training Time (s)')
     ax.set_title('Training Time Comparison with Different Adjustments')
     ax.set_xticks(x)
-    ax.set_xticklabels(models)
+    ax.set_xticklabels(data_dict["ratios"])
     ax.legend()
 
     fig.tight_layout()
@@ -62,4 +74,4 @@ def generate_graph():
     plt.show()
 
 
-generate_graph()
+populate_parameter_list()
