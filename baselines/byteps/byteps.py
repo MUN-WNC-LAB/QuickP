48e7870ece973affdc37756af366c118d15de4d9
hola <hongw@mun.ca>
2024-08-04 15:26:34 -0230

    fail to install pyteps

--- baselines/byteps/byteps.py
+++ baselines/byteps/byteps.py
@@ -1,5 +1,7 @@
-# sudo apt-get install -y libnccl2 libnccl-dev
-#cp /usr/include/nccl.h /usr/local/cuda/include/nccl.h
-# ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/local/cuda-11.0/nccl/lib/libnccl.so.2
-# export BYTEPS_NCCL_HOME=/path/to/nccl
+# sudo apt install libnccl2=2.20.3-1+cuda12.3 libnccl-dev=2.20.3-1+cuda12.3
+# dpkg -L libnccl2 libnccl-dev
+# sudo find / -name "*libnccl*"
+# cp /usr/include/nccl.h /usr/local/cuda-12.3/include/nccl.h
+# ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/local/cuda-12.3/nccl/lib/libnccl.so.2
+# export BYTEPS_NCCL_HOME=/usr
 # pip3 install byteps
