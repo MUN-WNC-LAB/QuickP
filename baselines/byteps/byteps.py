14d3d7bb185af88acc3cf29b7ac9ce7b80921f0d
hola <hongw@mun.ca>
2024-08-03 23:14:56 -0230

    byteps

---
+++ baselines/byteps/byteps.py
@@ -0,0 +1,5 @@
+# sudo apt-get install -y libnccl2 libnccl-dev
+#cp /usr/include/nccl.h /usr/local/cuda/include/nccl.h
+# ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/local/cuda-11.0/nccl/lib/libnccl.so.2
+# export BYTEPS_NCCL_HOME=/path/to/nccl
+# pip3 install byteps
--- baselines/flexflow/flexflow.py
+++ baselines/flexflow/flexflow.py
@@ -1,4 +1,10 @@
 # sudo apt-get -y install cudnn9-cuda-12
+# cat /usr/include/cudnn_version.h | grep CUDNN_MAJOR -A 2
+# "which nvcc" to get the right path. Eg: /usr/local/cuda-12.3/bin/nvcc
+# Copy the Cudnn file to the Cuda file address
+# sudo cp /usr/include/cudnn_version.h /usr/local/cuda-12.3/include
+# sudo cp /usr/lib/x86_64-linux-gnu/libcudnn* /usr/local/cuda-12.3/lib64
+# sudo chmod a+r /usr/local/cuda-12.3/lib64/libcudnn*
 # sudo apt install curl
 # apt install cmake ninja-build
 # specify CUDNN_DIR on .bashrc
