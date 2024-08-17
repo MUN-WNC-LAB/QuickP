b2194c2ba69baa701169d94ac2e2b2f3c79807f6
hola <hongw@mun.ca>
2024-08-16 22:55:40 -0230

    add proority queue scheduling

--- optimizer/optimization_problems/scheduling.py
+++ optimizer/optimization_problems/scheduling.py
@@ -1,6 +1,7 @@
 from collections import deque
 from enum import Enum
 from itertools import combinations
+from queue import PriorityQueue
 
 import networkx as nx
 from gurobipy import Model, GRB
@@ -116,8 +117,112 @@ def FIFO_scheduling(model: Model, start, finish, comm_start, comm_end, comp_grap
                         source_subgraph = partition_dict[predecessor]
                         last_communication_dict[source_subgraph] = (predecessor, task)
                         last_communication_dict[subgraph_id] = (predecessor, task)
-                        print("fuck", last_communication_dict[source_subgraph], last_communication_dict[subgraph_id])
 
+                # Track the finish time of the current task
+                last_job_dict[subgraph_id] = task
+
+                # Track task completion
+                completed_tasks.add(task)
+
+                # Update the queue based on the completion of the task
+                update_queue(device_queues, task, comp_graph, completed_tasks, partition_dict)
+
+    # Get the collection of nodes that are in the graph but not in completed_tasks
+    all_nodes = set(comp_graph.nodes())
+    remaining_nodes = all_nodes - completed_tasks
+    assert len(remaining_nodes) == 0, f"the remaining nodes {remaining_nodes} but all nodes should be scheduled"
+
+
+def priority_queue_scheduling(model: Model, start, finish, comm_start, comm_end, comp_graph: CompGraph,
+                              subgraph_dict: dict,
+                              edge_cut_list: list, partition_dict: dict):
+    def initialize_queues(subgraph_dict, dependency_graph) -> dict[any, PriorityQueue]:
+        # Initialize a queue for each subgraph (device)
+        device_queues = {subgraph_id: PriorityQueue() for subgraph_id, subgraph in subgraph_dict.items()}
+
+        # Initialize with tasks that have no predecessors in the global graph
+        for subgraph_id, subgraph in subgraph_dict.items():
+            for operator_id in subgraph.nodes():
+                # Check if the node has no predecessors in the global dependency graph
+                global_predecessors = list(dependency_graph.predecessors(operator_id))
+
+                # If the node has no predecessors in the global graph, it can be added to the queue
+                if not global_predecessors:
+                    # Add to the appropriate subgraph's queue
+                    device_queues[subgraph_id].put((1, operator_id))
+
+        return device_queues
+
+    def update_queue(device_queues: dict[any, PriorityQueue], finished_task, dependency_graph, completed_tasks,
+                     partition_dict):
+        # Check all successors of the finished task in the global dependency graph
+        successors = list(dependency_graph.successors(finished_task))
+        for succ in successors:
+            # Check if all predecessors are complete in the global dependency graph
+            predecessors = list(dependency_graph.predecessors(succ))
+            if all(predecessor in completed_tasks for predecessor in predecessors):
+                # Enqueue the task to the task queue of this subgraph (device)
+                subgraph_of_succ = partition_dict[succ]
+                if subgraph_of_succ != partition_dict[finished_task]:
+                    print(
+                        f"succ {succ} belongs to {subgraph_of_succ} while the current graph is {partition_dict[finished_task]}")
+                # cannot use "if subgraph_of_succ" since subgraph id can be 0
+                if subgraph_of_succ is not None:
+                    # Enqueue the task to the task queue of the correct subgraph (device)
+                    device_queues[subgraph_of_succ].put((1, succ))
+
+    ready = model.addVars(comp_graph.getOperatorIDs(), vtype=GRB.CONTINUOUS, lb=0.0,
+                          name="ready")  # ready[node_id] represent the ready time of this node, simulating Queue
+
+    for node in comp_graph.nodes():
+        for predecessor in comp_graph.predecessors(node):
+            model.addConstr(ready[node] >= finish[predecessor], name=f"fifo_{predecessor}_to_{node}")
+
+    # It is an SCHEDULING problem within each device.
+    device_queues = initialize_queues(subgraph_dict, comp_graph)
+    total_items = sum(queue.qsize() for queue in device_queues.values())
+    print("len of the init: ", total_items, 'The init device_queues is ', device_queues)
+
+    # Initialize the set to track completed tasks
+    completed_tasks = set()
+
+    # This list will store all the constraints that we batch before optimization
+    last_job_dict = {subgraph_id: None for subgraph_id in subgraph_dict.keys()}
+    last_communication_dict = {subgraph_id: None for subgraph_id in subgraph_dict.keys()}
+    # Process each subgraph independently
+    while any(not pqueue.empty() for pqueue in device_queues.values()):
+        for subgraph_id, queue in device_queues.items():
+            if not queue.empty():
+                # Get the next task to execute for this subgraph
+                _, task = queue.get()
+
+                # check if this task get completed
+                if task in completed_tasks:
+                    raise ValueError("this is a repeated task")
+
+                # check if all dependency satisfy
+                for predecessor in nx.ancestors(comp_graph, task):
+                    if predecessor not in completed_tasks:
+                        raise ValueError(f"{task} 's dependency {predecessor} not satisfied")
+
+                # Ensure the task starts after its ready time
+                model.addConstr(start[task] >= ready[task],
+                                name=f"start_after_ready_{task}_on_subgraph_{subgraph_id}")
+
+                # Ensure that the task starts after the previous task finishes within the same subgraph
+                # Operator scheduling within device
+                if last_job_dict[subgraph_id] is not None:
+                    model.addConstr(start[task] >= finish[last_job_dict[subgraph_id]],
+                                    name=f"start_after_prev_finish_{task}_on_subgraph_{subgraph_id}")
+
+                # Communication Scheduling. One device can only send or receive from up to one link at the same time
+                for predecessor in comp_graph.predecessors(task):
+                    if (predecessor, task) in edge_cut_list:
+                        if last_communication_dict[subgraph_id] is not None:
+                            model.addConstr(comm_start[predecessor, task] >= comm_end[last_communication_dict[subgraph_id]])
+                        source_subgraph = partition_dict[predecessor]
+                        last_communication_dict[source_subgraph] = (predecessor, task)
+                        last_communication_dict[subgraph_id] = (predecessor, task)
 
                 # Track the finish time of the current task
                 last_job_dict[subgraph_id] = task
