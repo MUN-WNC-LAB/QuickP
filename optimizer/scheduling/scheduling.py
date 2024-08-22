d58197b39c236531d766dcf26101f90198cdfd05
hola <hongw@mun.ca>
2024-08-22 17:16:03 -0230

    add solver based FIFO

--- optimizer/scheduling/scheduling.py
+++ optimizer/scheduling/scheduling.py
@@ -295,11 +295,33 @@ def optimal_scheduling_with_prob_function(model: Model, start, finish, comm_star
     pass
 
 
+def FIFO_scheduling_solver(model: Model, start, finish, comm_start, comm_end, comp_graph: CompGraph,
+                    device_subgraph_mapping: dict, edge_cut_list: list, operator_device_mapping: dict):
+    M = 1000000
+    order = {}
+    ready = model.addVars(comp_graph.getOperatorIDs(), vtype=GRB.CONTINUOUS, lb=0.0,
+                          name="ready")  # ready[node_id] represent the ready time of this node, simulating Queue
+
+    for node in comp_graph.nodes():
+        for predecessor in comp_graph.predecessors(node):
+            model.addConstr(ready[node] >= finish[predecessor], name=f"fifo_{predecessor}_to_{node}")
+
+    for subgraph in device_subgraph_mapping.values():
+        for source_op, dest_op in subgraph.getEdgeIDs():
+            model.addConstr(finish[source_op] <= start[dest_op])
+        non_connected_pairs = find_non_connected_pairs(subgraph)
+        for op_a, op_b in non_connected_pairs:
+            order[op_a, op_b] = model.addVar(vtype=GRB.BINARY, name=f"order_{op_a}_{op_b}")
+            model.addConstr(start[op_b] >= finish[op_a] - M * (1 - order[op_a, op_b]), name=f"NoOverlap1_{op_a}_{op_b}")
+            model.addConstr(start[op_a] >= finish[op_b] - M * order[op_a, op_b], name=f"NoOverlap2_{op_a}_{op_b}")
+
+
 class SchedulingAlgorithm(Enum):
     FIFO = "FIFO"
     OPTIMIZED = "OPTIMIZED"
     PRIORITY_QUEUE = "PRIORITY_QUEUE"
     NEAR_OPTIMAL = "NEAR_OPTIMAL"
+    FIFO_SOLVER = "FIFO_SOLVER"
 
 
 def execute_scheduling_function(sch_fun_type: str, model: Model, **kwargs):
