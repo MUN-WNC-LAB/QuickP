6f7c230f3be36960a1707a5c6352648304835e88
hola <hongw@mun.ca>
2024-08-25 16:14:40 -0230

    seems fix bug for new opt

--- optimizer/scheduling/near_opt.py
+++ optimizer/scheduling/near_opt.py
@@ -28,11 +28,11 @@ def near_optimal_scheduling_revised(model: Model, start, finish, comm_start, com
         other_nodes = sorted(other_nodes, key=lambda op: node_order_dict[op])
 
         for high_cost_node in high_cost_nodes:
-            non_connected_chain_list = get_non_connected_list_by_operator(comp_graph, high_cost_node)
-            for op_a, op_b in itertools.combinations(non_connected_chain_list, 2):
-                order[op_a, op_b] = model.addVar(vtype=GRB.BINARY, name=f"order_{op_a}_{op_b}")
-                model.addConstr(start[op_b] >= finish[op_a] - M * (1 - order[op_a, op_b]), name=f"NoOverlap1_{op_a}_{op_b}")
-                model.addConstr(start[op_a] >= finish[op_b] - M * order[op_a, op_b], name=f"NoOverlap2_{op_a}_{op_b}")
+            non_connected_chain_list = get_non_connected_list_by_operator(subgraph, high_cost_node)
+            for non_connected_op in non_connected_chain_list:
+                order[high_cost_node, non_connected_op] = model.addVar(vtype=GRB.BINARY, name=f"order_{high_cost_node}_{non_connected_op}")
+                model.addConstr(start[non_connected_op] >= finish[high_cost_node] - M * (1 - order[high_cost_node, non_connected_op]), name=f"NoOverlap1_{high_cost_node}_{non_connected_op}")
+                model.addConstr(start[high_cost_node] >= finish[non_connected_op] - M * order[high_cost_node, non_connected_op], name=f"NoOverlap2_{high_cost_node}_{non_connected_op}")
         for op_a, op_b in zip(other_nodes, other_nodes[1:]):
             model.addConstr(finish[op_a] <= start[op_b])
 
