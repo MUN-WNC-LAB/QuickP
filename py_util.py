0b9e54f151004c0fb86413f01cad8aabe70205fc
hola <hongw@mun.ca>
2024-09-19 14:21:59 -0230

    delete log

--- py_util.py
+++ py_util.py
@@ -272,7 +272,6 @@ def tensor_shape_to_bits(tensor_shape, dtype: DType):
     :return: Total size in bits.
     """
     # Get the total number of elements
-    print(tensor_shape, dtype)
     if None in tensor_shape:
         return 0
     total_elements = tf.reduce_prod(tensor_shape).numpy()
