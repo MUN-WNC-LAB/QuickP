c280e8c46922a0e58f3dd19c2918450ed4711ccd
Hong Wang <hongw@mun.ca>
2024-09-30 01:13:57 -0230

    init quicks list sch

---
+++ optimizer/scheduling/quicks/quicks.py
@@ -0,0 +1,95 @@
+import networkx as nx
+from gurobipy import Model, GRB
+from networkx import Graph
+
+from optimizer.model.graph import CompGraph
+from optimizer.scheduling.near_optimal_scheduling_with_sampling import get_device_unreachable_pairs_mapping, split_nodes
+from optimizer.scheduling.scheduling_util import three_stage_split_subgraph, split_three_stage_subgraph
+
+
+def quickS(model: Model, start, finish, comm_start, comm_end, comp_graph: CompGraph,
+           device_subgraph_mapping: dict, edge_cut_list: list, operator_device_mapping: dict,
+           rho, sampling_function):
+    rank_map = optimize_dependent_relied_part(model, start, finish, comm_start, comm_end, comp_graph,
+                                              device_subgraph_mapping, edge_cut_list, operator_device_mapping,
+                                              rho, sampling_function)
+
+
+def optimize_dependent_relied_part(model: Model, start, finish, comm_start, comm_end, comp_graph: CompGraph,
+                                   device_subgraph_mapping: dict, edge_cut_list: list, operator_device_mapping: dict,
+                                   rho, sampling_function):
+    rank_map = {}
+    M = 1000000
+    topological_order = list(nx.topological_sort(comp_graph))
+    topological_order_mapping = {node: index for index, node in enumerate(topological_order)}
+    order = {}
+    stage_to_be_optimize_mapping: dict[any, Graph] = {}
+    stage_two_finish = model.addVars(device_subgraph_mapping.keys(), vtype=GRB.CONTINUOUS, lb=0.0,
+                                     name="non_isolated_part_finish")
+
+    # form new device non-isolated part mapping
+    # split into isolated and non-isolated part
+    for device, subgraph in device_subgraph_mapping.items():
+        # Simply the search space by
+        stage_one, stage_two, stage_three = split_three_stage_subgraph(
+            subgraph, operator_device_mapping, edge_cut_list)
+        # give stage one the highest rank and the three the lowest rank
+        for stage_one_node in stage_one:
+            rank_map[stage_one_node] = 10000000
+        for stage_three_node in stage_three:
+            rank_map[stage_three_node] = 0
+
+        # Map stage two to device
+        stage_to_be_optimize_mapping[device] = stage_two
+
+        # we only care about
+        # independent relied part => random topo sort, this stage should always be first, we don't care know
+        stage_one_sorted = sorted(list(stage_one.nodes), key=lambda node: topological_order_mapping[node])
+        for a, b in zip(stage_one_sorted, stage_one_sorted[1:]):
+            model.addConstr(finish[a] <= start[b])
+        # stage_three => non-exporting part, FIFO is okay, we don't care
+        stage_three_sorted = sorted(list(stage_one.nodes), key=lambda node: topological_order_mapping[node])
+        for a, b in zip(stage_three_sorted, stage_three_sorted[1:]):
+            model.addConstr(finish[a] <= start[b])
+
+        # nonoverlapping between stage 1 and 2
+        for stage_two_node in stage_two:
+            model.addConstr(stage_two_finish[device] >= finish[stage_two_node])
+            model.addConstr(start[stage_two_node] >= finish[stage_one_sorted[-1]])
+        # # nonoverlapping between stage 2 and 3
+        model.addConstr(start[stage_three_sorted[0]] >= stage_two_finish[device])
+
+    device_unreachable_pairs_mapping, global_set_with_nr = get_device_unreachable_pairs_mapping(
+        stage_to_be_optimize_mapping)
+    global_node_split_by_device = split_nodes(comp_graph, global_set_with_nr, list(device_subgraph_mapping.keys()),
+                                              operator_device_mapping, r=rho,
+                                              sampling_function=sampling_function)
+
+    for device, non_iso_part in stage_to_be_optimize_mapping.items():
+
+        # flatten the pairs to get all the non-repeated nodes, convert to list
+        selected_nodes, other_nodes = global_node_split_by_device.get(device)["selected_list"], \
+            global_node_split_by_device.get(device)["unselected_list"]
+
+        # use the FIFO order to sort other_nodes;
+        local_fifo_order = fifo_operator_order[device]
+        node_order_dict = {op: idx for idx, op in enumerate(local_fifo_order)}
+        # sort other_nodes based on the FIFO order
+        other_nodes = sorted(other_nodes, key=lambda op: node_order_dict[op])
+        # Apply sequential constraint to non-selected nodes
+        for op_a, op_b in zip(other_nodes, other_nodes[1:]):
+            model.addConstr(finish[op_a] <= start[op_b])
+
+        # apply optimization to all pairs involving these high-cost nodes, these pair also include the unselected nodes,
+        # To optimize one node, all of its non-reachable node should be included
+        important_pairs = [pair for pair in device_unreachable_pairs_mapping[device] if
+                           pair[0] in selected_nodes or pair[1] in selected_nodes]
+        for op_a, op_b in important_pairs:
+            current_subgraph_dc = stage_to_be_optimize_mapping[device]
+            assert op_a in current_subgraph_dc.nodes and op_b in current_subgraph_dc.nodes
+            assert not nx.has_path(current_subgraph_dc, op_a, op_b)
+            order[op_a, op_b] = model.addVar(vtype=GRB.BINARY, name=f"order_{op_a}_{op_b}")
+            model.addConstr(start[op_b] >= finish[op_a] - M * (1 - order[op_a, op_b]), name=f"NoOverlap1_{op_a}_{op_b}")
+            model.addConstr(start[op_a] >= finish[op_b] - M * order[op_a, op_b], name=f"NoOverlap2_{op_a}_{op_b}")
+
+    return rank_map
---
+++ optimizer/scheduling/quicks/quicks_list_schedule.py
@@ -0,0 +1,119 @@
+from queue import PriorityQueue
+
+import networkx as nx
+from gurobipy import Model
+
+from optimizer.model.graph import CompGraph
+
+
+def priority_queue_max_rank_heteroG(model: Model, start, finish, comm_start, comm_end, comp_graph: CompGraph,
+                                 device_subgraph_mapping: dict, edge_cut_list: list, operator_device_mapping: dict):
+
+    global_rank = {}
+    topo_sorted = list(nx.topological_sort(comp_graph))
+
+    for current_node in reversed(topo_sorted):
+        # Check if the current node has any predecessors
+        successors = list(comp_graph.successors(current_node))
+
+        if successors:  # If there are predecessors, compute the max computing cost
+            max_suc_computing_cost = max(
+            global_rank[succ_node] for succ_node in successors
+        )
+        else:  # If there are no predecessors, set the max computing cost to 0
+            max_suc_computing_cost = 0
+
+        # Calculate the global rank for the current node
+        global_rank[current_node] = (
+                max_suc_computing_cost + comp_graph.getOperatorCompCostByDevice(current_node,
+                                                                            operator_device_mapping[current_node])
+        )
+
+    def initialize_queues(subgraph_dict, dependency_graph) -> dict[any, PriorityQueue]:
+        # Initialize a queue for each device
+        device_queue_dict = {device: PriorityQueue() for device, subgraph in subgraph_dict.items()}
+
+        # Initialize with tasks that have no predecessors in the global graph
+        for device, subgraph in subgraph_dict.items():
+            for operator_id in subgraph.nodes():
+                # Check if the node has no predecessors in the global dependency graph
+                global_predecessors = list(dependency_graph.predecessors(operator_id))
+
+                # If the node has no predecessors in the global graph, it can be added to the queue
+                if not global_predecessors:
+                    # Add to the appropriate subgraph's queue
+                    # must time -1 because the queue should dequeue the one with the max rank each time,
+                    # but priority_queue use min-heap
+                    device_queue_dict[device].put((global_rank[operator_id] * -1, operator_id))
+
+        return device_queue_dict
+
+    def update_queue(device_queue_dict: dict[any, PriorityQueue], finished_task, dependency_graph, completed_tasks,
+                     partition_dict):
+        # Check all successors of the finished task in the global dependency graph
+        successors = list(dependency_graph.successors(finished_task))
+        for successor in successors:
+            # Check if all predecessors are complete in the global dependency graph
+            predecessors = list(dependency_graph.predecessors(successor))
+            if all(predecessor in completed_tasks for predecessor in predecessors):
+                # Enqueue the task to the task queue of this subgraph (device)
+                device_of_successor = partition_dict[successor]
+                # cannot use "if subgraph_of_succ" since subgraph id can be 0
+                assert device_of_successor is not None
+                # Enqueue the task to the task queue of the correct subgraph (device)
+                device_queue_dict[device_of_successor].put((global_rank[successor] * -1, successor))
+
+    # It is an SCHEDULING problem within each device.
+    device_queues = initialize_queues(device_subgraph_mapping, comp_graph)
+    total_items = sum(queue.qsize() for queue in device_queues.values())
+    print("len of the init: ", total_items, 'The init device_queues is ', device_queues)
+
+    # Initialize the set to track completed tasks
+    completed_tasks = set()
+
+    # This list will store all the constraints that we batch before optimization
+    last_job_dict = {subgraph_id: None for subgraph_id in device_subgraph_mapping.keys()}
+    last_communication_dict = {subgraph_id: None for subgraph_id in device_subgraph_mapping.keys()}
+    # Process each subgraph independently
+    while any(not pqueue.empty() for pqueue in device_queues.values()):
+        for device_id, queue in device_queues.items():
+            if not queue.empty():
+                # Get the next task to execute for this subgraph
+                _, task = queue.get()
+
+                # check if this task get completed
+                if task in completed_tasks:
+                    raise ValueError("this is a repeated task")
+
+                # check if all dependency satisfy
+                for predecessor in nx.ancestors(comp_graph, task):
+                    if predecessor not in completed_tasks:
+                        raise ValueError(f"{task} 's dependency {predecessor} not satisfied")
+
+                # Ensure that the task starts after the previous task finishes within the same subgraph
+                # Operator scheduling within device
+                if last_job_dict[device_id] is not None:
+                    model.addConstr(start[task] >= finish[last_job_dict[device_id]], name=f"start_after_prev_finish_{task}_on_subgraph_{device_id}")
+                '''
+                # Communication scheduling. One device can only send or receive from up to one link at the same time
+                for predecessor in comp_graph.predecessors(task):
+                    if (predecessor, task) in edge_cut_list:
+                        if last_communication_dict[device_id] is not None:
+                            model.addConstr(comm_start[predecessor, task] >= comm_end[last_communication_dict[device_id]])
+                        source_device = operator_device_mapping[predecessor]
+                        last_communication_dict[source_device] = (predecessor, task)
+                        last_communication_dict[device_id] = (predecessor, task)
+                '''
+                # Track the finish time of the current task
+                last_job_dict[device_id] = task
+
+                # Track task completion
+                completed_tasks.add(task)
+
+                # Update the queue based on the completion of the task
+                update_queue(device_queues, task, comp_graph, completed_tasks, operator_device_mapping)
+
+    # Get the collection of nodes that are in the graph but not in completed_tasks
+    all_nodes = set(comp_graph.nodes())
+    remaining_nodes = all_nodes - completed_tasks
+    assert len(remaining_nodes) == 0, f"the remaining nodes {remaining_nodes} but all nodes should be scheduled"
