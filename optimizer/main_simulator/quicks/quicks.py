b9bc59388ba540a42e17de7d75cb4e5591503542
Hong Wang <hongw@mun.ca>
2024-09-30 01:52:00 -0230

    need reinit the model 2

--- optimizer/main_simulator/quicks/quicks.py
+++ optimizer/main_simulator/quicks/quicks.py
@@ -2,18 +2,27 @@ import networkx as nx
 from gurobipy import Model, GRB
 from networkx import Graph
 
+from DNN_model_tf.tf_model_enum import TFModelEnum
+from optimizer.main_simulator.gurobi_util import init_computing_and_device_graph
+from optimizer.main_simulator.quicks.cost_model_quicks import evaluate_quicks
 from optimizer.model.graph import CompGraph
+from optimizer.operator_device_placement.metis.subgraph_util import WeightNormalizationFunction, init_graph_weight, \
+    construct_sub_graph
+from optimizer.operator_device_placement.metis.weight_functions import NodeWeightFunction, EdgeWeightFunction
+from optimizer.operator_device_placement.placement import get_placement_info
 from optimizer.scheduling.near_optimal_scheduling_with_sampling import get_device_unreachable_pairs_mapping, split_nodes
-from optimizer.scheduling.quicks.quicks_list_schedule import quicks_list_schedule
-from optimizer.scheduling.scheduling_util import three_stage_split_subgraph, split_three_stage_subgraph
+from optimizer.main_simulator.quicks.quicks_list_schedule import quicks_list_schedule
+from optimizer.scheduling.scheduling_util import split_three_stage_subgraph
 
 
-def quickS(model: Model, start, finish, comm_start, comm_end, comp_graph: CompGraph,
-           device_subgraph_mapping: dict, edge_cut_list: list, operator_device_mapping: dict,
-           rho, sampling_function):
-    rank_map = optimize_dependent_relied_part(model, start, finish, comm_start, comm_end, comp_graph,
-                                              device_subgraph_mapping, edge_cut_list, operator_device_mapping,
-                                              rho, sampling_function)
+def quickS(comp_graph: CompGraph, deviceTopo):
+    # Partition the computation graph
+    operator_device_mapping, edge_cut_list, edge_cut_weight_sum = (
+        get_placement_info("METIS", comp_graph, deviceTopo))
+    # Update the op_id-subgraph_id mapping dict to op_id-device_id mapping dict
+    device_subgraph_mapping = construct_sub_graph(comp_graph, operator_device_mapping)
+
+    rank_map = evaluate_quicks(comp_graph, deviceTopo, operator_device_mapping, edge_cut_list)
     quicks_list_schedule(model, start, finish, comm_start, comm_end, comp_graph,
                          device_subgraph_mapping, operator_device_mapping, rank_map)
 
@@ -96,3 +105,22 @@ def optimize_dependent_relied_part(model: Model, start, finish, comm_start, comm
             model.addConstr(start[op_a] >= finish[op_b] - M * order[op_a, op_b], name=f"NoOverlap2_{op_a}_{op_b}")
 
     return rank_map
+
+
+if __name__ == '__main__':
+    graph_init = {
+        "number_of_devices": 6,
+        "model_type": TFModelEnum.ALEXNET,
+        "node_weight_function": NodeWeightFunction.AVE_COMP_COST,
+        "edge_weight_function": EdgeWeightFunction.SOURCE_OUTPUT_TENSOR,
+        "weight_norm_function": WeightNormalizationFunction.MIN_MAX,
+    }
+
+    # init fake data
+    deviceTopo, comp_graph = init_computing_and_device_graph(graph_init["number_of_devices"], None,
+                                                             model_type=graph_init["model_type"])
+    # init graph node/edge weight
+    init_graph_weight(comp_graph, graph_init["node_weight_function"], graph_init["edge_weight_function"],
+                      graph_init["weight_norm_function"])
+
+    quickS(comp_graph, deviceTopo)
