339ff3939a00e467a2eaef83061446c043d5516a
hola <hongw@mun.ca>
2024-10-04 22:34:12 -0230

    change accuracy

--- optimizer/main_simulator/gurobi_util.py
+++ optimizer/main_simulator/gurobi_util.py
@@ -18,7 +18,7 @@ def gurobi_setup(name: str):
     model = Model(name)
     model.setParam("LogToConsole", 0)
     model.setParam("LogFile", "gurobi.log")
-    model.setParam("MIPGap", 0.11)
+    model.setParam("MIPGap", 0.01)
     model.setParam("TimeLimit", 2400)
     model.setParam("MIPFocus", 1)
 
