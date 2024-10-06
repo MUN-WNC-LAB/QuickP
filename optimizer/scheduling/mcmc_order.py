80bf936d04fcb116b4a1259f0f8600962d461590
Hong Wang <hongw@mun.ca>
2024-10-06 01:57:27 -0230

    delete mcmc

--- optimizer/scheduling/mcmc_order.py
+++
@@ -1,8 +0,0 @@
-# MCMC searching logic is implemented in MCMC.py. Here is to apply the sequential order constraint
-from gurobipy import Model
-
-
-def mcmc_schedule(model: Model, start, finish, comm_start, comm_end, mcmc_order_dict: dict):
-    for sequence in mcmc_order_dict.values():
-        for a, b in zip(sequence, sequence[1:]):
-            model.addConstr(finish[a] <= start[b])
