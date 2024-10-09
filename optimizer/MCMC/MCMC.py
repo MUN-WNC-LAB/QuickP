7f6ab23012e63c2384a83790a0938a8a855f97f7
hola <hongw@mun.ca>
2024-10-09 18:21:26 -0230

    add back MCMC but does not make sense

---
+++ optimizer/MCMC/MCMC.py
@@ -0,0 +1,67 @@
+import random
+
+from DNN_model_tf.tf_model_enum import TFModelEnum
+from optimizer.MCMC.cost_model import evaluate_mcmc
+from optimizer.main_simulator.gurobi_util import init_computing_and_device_graph
+from optimizer.model.graph import CompGraph, find_non_connected_pairs
+from optimizer.operator_device_placement.metis.subgraph_util import WeightNormalizationFunction, init_graph_weight, \
+    construct_sub_graph
+from optimizer.operator_device_placement.metis.weight_functions import NodeWeightFunction, EdgeWeightFunction
+from optimizer.operator_device_placement.placement import get_placement_info
+from optimizer.scheduling.scheduling_order_only import FIFO_scheduling_order
+
+
+def mcmc_search(comp_graph: CompGraph, deviceTopo):
+    all_non_connected_pairs = []
+    # Partition the computation graph
+    operator_device_mapping, edge_cut_list, edge_cut_weight_sum = (
+        get_placement_info("METIS", comp_graph, deviceTopo))
+    # Update the op_id-subgraph_id mapping dict to op_id-device_id mapping dict
+    device_subgraph_mapping = construct_sub_graph(comp_graph, operator_device_mapping)
+
+    # Execute the simulation
+    init_latency = evaluate_mcmc(comp_graph, deviceTopo, operator_device_mapping, edge_cut_list)
+
+    current_strategy = {"placement": operator_device_mapping, "latency": init_latency}
+
+    for device, subgraph in device_subgraph_mapping.items():
+        # there will be no pairs with the same element
+        non_connected_pairs = find_non_connected_pairs(subgraph)
+        all_non_connected_pairs.extend(non_connected_pairs)
+
+    for i in range(0, 10):
+        random_tuple = random.choice(all_non_connected_pairs)
+        assigned_device = operator_device_mapping[random_tuple[0]]
+        new_strategy = current_strategy["scheduling"].copy()
+        assigned_sequence = new_strategy[assigned_device]
+        # Find the indices of the two nodes
+        index1 = assigned_sequence.index(random_tuple[0])
+        index2 = assigned_sequence.index(random_tuple[1])
+
+        # Swap the two nodes using multiple assignment
+        assigned_sequence[index1], assigned_sequence[index2] = assigned_sequence[index2], assigned_sequence[index1]
+        new_latency = evaluate_mcmc(comp_graph, deviceTopo, operator_device_mapping, edge_cut_list, init_order_dict)
+        if new_latency < current_strategy["latency"]:
+            current_strategy["scheduling"] = new_strategy
+            current_strategy["latency"] = new_latency
+
+    print(current_strategy["latency"])
+
+
+if __name__ == '__main__':
+    graph_init = {
+        "number_of_devices": 6,
+        "model_type": TFModelEnum.ALEXNET,
+        "node_weight_function": NodeWeightFunction.AVE_COMP_COST,
+        "edge_weight_function": EdgeWeightFunction.SOURCE_OUTPUT_TENSOR,
+        "weight_norm_function": WeightNormalizationFunction.MIN_MAX,
+    }
+
+    # init fake data
+    deviceTopo, comp_graph = init_computing_and_device_graph(graph_init["number_of_devices"], None,
+                                                             model_type=graph_init["model_type"])
+    # init graph node/edge weight
+    init_graph_weight(comp_graph, graph_init["node_weight_function"], graph_init["edge_weight_function"],
+                      graph_init["weight_norm_function"])
+
+    mcmc_search(comp_graph, deviceTopo)
---
+++ optimizer/MCMC/cost_model.py
@@ -0,0 +1,116 @@
+from gurobipy import *
+
+from optimizer.main_simulator.simulator_util import get_comp_cost_dict, get_comm_cost_dict
+from optimizer.model.graph import CompGraph, DeviceGraph
+
+os.environ['GRB_LICENSE_FILE'] = '/home/hola/solverLicense/gurobi.lic'
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(script_dir, '..', '..'))
+sys.path.append(project_root)
+from optimizer.operator_device_placement.metis.subgraph_util import construct_sub_graph, WeightNormalizationFunction, \
+    init_graph_weight
+from optimizer.main_simulator.gurobi_util import init_computing_and_device_graph, gurobi_setup, \
+    show_optimization_solution, show_graph_partition_info
+
+
+def evaluate_mcmc(computing_graph: CompGraph, device_topo: DeviceGraph, operator_device_mapping, edge_cut_list, sch_order_dict):
+
+    # Update the op_id-subgraph_id mapping dict to op_id-device_id mapping dict
+    device_subgraph_mapping = construct_sub_graph(computing_graph, operator_device_mapping)
+
+    # Get computation and communication cost
+    op_computing_cost_mapping = get_comp_cost_dict(computing_graph, operator_device_mapping)
+    edge_cut_communication_cost_mapping = get_comm_cost_dict(computing_graph, device_topo, edge_cut_list, operator_device_mapping)
+
+    # two_dime_node_list is to test whether the
+    two_dime_node_list: list[list] = [list(subgraph.nodes.keys()) for subgraph in device_subgraph_mapping.values()]
+
+    # Init solver
+    model = gurobi_setup("minimize_maxload")
+
+    # Define variables
+
+    start = model.addVars(computing_graph.getOperatorIDs(), vtype=GRB.CONTINUOUS, lb=0.0,
+                          name="start")  # start[node_id] represent the starting time of this node
+    finish = model.addVars(computing_graph.getOperatorIDs(), vtype=GRB.CONTINUOUS, lb=0.0,
+                           name="finish")  # finish[node_id] represent the finish time of this node
+    comm_start = model.addVars(edge_cut_list, vtype=GRB.CONTINUOUS, lb=0.0,
+                               name="")  # comm_start[source_op, dest_op] represent the communication
+    comm_end = model.addVars(edge_cut_list, vtype=GRB.CONTINUOUS, lb=0.0, name="")
+
+    '''
+    Define Constraints
+    '''
+
+    for node_id in computing_graph.getOperatorIDs():
+        # Add constraints that each op's ending time = starting time + its computing time
+        model.addConstr(finish[node_id] == start[node_id] + op_computing_cost_mapping[node_id], name=f"finish_start_{node_id}")
+
+    # Data dependency for same-device communication
+    non_edge_cut_list = [edge for edge in computing_graph.getEdgeIDs() if edge not in edge_cut_list]
+    for edge_id_tuple in non_edge_cut_list:
+        source_op_ID, target_op_ID = edge_id_tuple
+        model.addConstr(finish[source_op_ID] <= start[target_op_ID])
+
+    # Data dependency for cross-device communication
+    for edge_id_tuple in edge_cut_list:
+        # only the edge in the edge_cut_list will bring communication cost since the source_op and destination-op are
+        # placed on different devices
+        source_op_ID, dest_op_ID = edge_id_tuple
+        # Ensures the communication starts only after the source operation finishes.
+        model.addConstr(finish[source_op_ID] <= comm_start[source_op_ID, dest_op_ID],
+                        name = "")
+        # Ensures the communication duration covers the communication cost.
+        model.addConstr(comm_start[source_op_ID, dest_op_ID] + edge_cut_communication_cost_mapping[edge_id_tuple] == comm_end[source_op_ID, dest_op_ID],
+                        name = "")
+        # Ensures the communication ends before the destination operation starts.
+        model.addConstr(comm_end[source_op_ID, dest_op_ID] <= start[dest_op_ID],
+                        name = "")
+
+    # It is an SCHEDULING problem within each device.
+    mcmc_schedule(model, start=start, finish=finish, comm_start=comm_start,
+                                comm_end=comm_end, mcmc_order_dict=sch_order_dict)
+
+    # TotalLatency that we are minimizing
+    TotalLatency = model.addVar(vtype=GRB.CONTINUOUS, lb=0.0)
+    for op_end in finish.values():
+        model.addConstr(TotalLatency >= op_end, "satisfy each deice's latency")
+
+    # Set the target of solver
+    model.setObjective(TotalLatency, GRB.MINIMIZE)
+
+    # Run the solver
+    sys.stdout.flush()
+    model.optimize()
+
+    # Check optimization status
+    if model.status == GRB.INFEASIBLE:
+        print("Model is infeasible. Computing IIS...")
+        model.computeIIS()
+        model.write("model.ilp")
+        print("IIS written to model.ilp")
+
+        # Print the constraints that are in the IIS
+        print("\nThe following constraints are in the IIS:")
+        for constr in model.getConstrs():
+            if constr.IISConstr:
+                print(f"{constr.ConstrName}")
+        if model is not None:
+            model.dispose()
+        disposeDefaultEnv()
+    elif model.status == GRB.UNBOUNDED:
+        print("Model is unbounded.")
+    # this is the main process part after a solution is reached
+    elif model.status == GRB.OPTIMAL:
+        # show_optimization_solution(model, operator_device_mapping, computing_graph, device_topo, start, finish, edge_cut_communication_cost_mapping, True, two_dime_node_list)
+        optimal_value = model.ObjVal
+        if model is not None:
+            model.dispose()
+        disposeDefaultEnv()
+        return optimal_value
+    else:
+        print(f"Optimization ended with status {model.status}")
+        if model is not None:
+            model.dispose()
+        disposeDefaultEnv()
