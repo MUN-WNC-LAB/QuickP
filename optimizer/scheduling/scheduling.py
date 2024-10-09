0a352db21be39bd82c533565764f240a18ae7b41
hola <hongw@mun.ca>
2024-10-09 15:47:54 -0230

    the colocation really simply the calculation

--- optimizer/scheduling/scheduling.py
+++ optimizer/scheduling/scheduling.py
@@ -15,13 +15,13 @@ from optimizer.scheduling.priority_heteroG import priority_queue_max_rank_hetero
 from optimizer.scheduling.priority_min_comp_cost import priority_queue_min_comp_cost
 
 
-def add_topo_order_constraints(model, graph, x, device_ids, finish, start, group_ops_mapping, M):
+def add_topo_order_constraints(model, graph, x, device_ids, finish, start, group_ops_mapping: dict, M):
     op_group_mapping = get_op_group_map(group_ops_mapping)
     topological_order_mapping = {node: index for index, node in enumerate(list(nx.topological_sort(graph)))}
     non_reachable_pairs = find_non_connected_pairs(graph)
     ungrouped_non_reachable_pairs = [(a,b) for (a,b) in non_reachable_pairs if a in op_group_mapping and b in op_group_mapping
                                      and op_group_mapping[a] == op_group_mapping[b]]
-    for group_id, group in group_ops_mapping:
+    for group_id, group in group_ops_mapping.items():
         group = sorted(group, key=lambda node: topological_order_mapping[node])
         for a, b in zip(group, group[1:]):
             model.addConstr(finish[a] <= start[b])
