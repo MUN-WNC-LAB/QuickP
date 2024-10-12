dc30ae3a19b7f3dd958f48eb50201a876d965e51
hola <hongw@mun.ca>
2024-10-12 17:06:34 -0230

    op merge init 2

--- optimizer/co_location_and_merge/group_algorithm.py
+++ optimizer/co_location_and_merge/group_algorithm.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from networkx.classes import DiGraph
 
 from optimizer.co_location_and_merge.grouper_util import merge_group, label_all_node_with_group, edge_based_label
@@ -21,6 +23,40 @@ def group_and_merge_group(computing_graph: CompGraph, device_topo: DeviceGraph):
 
 def merge_operators(computing_graph: CompGraph, operator_2d_list, computing_cost_dict):
 
+    # _generate_fused_op_graph
     def generate_new_operator(ops_to_be_merged):
-        pass
+        new_computing_cost = sum(computing_cost_dict[op] for op in ops_to_be_merged)
+
+        internal_edges = deque(
+            [(u, v) for u, v in computing_graph.edges()
+             if u in ops_to_be_merged and v in ops_to_be_merged])
+        while len(internal_edges) > 0:
+            op1, op2 = internal_edges.popleft()
+
+
+        # Add the new node to the graph
+        computing_graph.add_node(new_node)
+
+        # Redirect all incoming edges to the new node, avoiding duplicates
+        predecessors = set()
+        for node in ops_to_be_merged:
+            predecessors.update(computing_graph.predecessors(node))  # Collect unique predecessors
+
+        for pred in predecessors:
+            if not computing_graph.has_edge(pred, new_node):  # Avoid adding duplicate edges
+                computing_graph.add_edge(pred, new_node)
+
+        # Redirect all outgoing edges from the merged nodes to the new node, avoiding duplicates
+        successors = set()
+        for node in ops_to_be_merged:
+            successors.update(computing_graph.successors(node))  # Collect unique successors
+
+        for succ in successors:
+            if not computing_graph.has_edge(new_node, succ):  # Avoid adding duplicate edges
+                computing_graph.add_edge(new_node, succ)
+
+        # Remove the original nodes
+        computing_graph.remove_nodes_from(ops_to_be_merged)
+
+
 
