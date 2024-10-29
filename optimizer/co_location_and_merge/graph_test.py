b4870f061908c2b78ce44278841af464e1bfc59e
hola <hongw@mun.ca>
2024-10-29 00:39:18 -0230

    comment update

--- optimizer/co_location_and_merge/graph_test.py
+++ optimizer/co_location_and_merge/graph_test.py
@@ -96,16 +96,16 @@ def sp_cal_():
 
     min_rank_calculation(comp_graph, deviceTopo)
 '''
-        elif (comp_graph.getOperatorCompCostByDevice(u,random_device) == 0 and comp_graph.getOperatorCompCostByDevice(v,random_device) == 0):
+        elif (comp_graph.getOperatorCompCostByDevice(u,random_device) <= 10 and comp_graph.getOperatorCompCostByDevice(v, random_device) <= 10):
             data = comp_graph.merge_edge(u, v)
             update_shortest_path_cost(comp_graph, u, device_topo, fast_link)
-        elif comp_graph.getOperatorCompCostByDevice(u, random_device) == 0 and comp_graph.out_degree(u) == 1:
+        elif comp_graph.getOperatorCompCostByDevice(u, random_device) <= 10 and comp_graph.out_degree(u) == 1:
             data = comp_graph.merge_edge(u, v)
             update_shortest_path_cost(comp_graph, u, device_topo, fast_link)
-        elif comp_graph.getOperatorCompCostByDevice(v, random_device) == 0 and comp_graph.in_degree(v) == 1:
+        elif comp_graph.getOperatorCompCostByDevice(v, random_device) <= 10 and comp_graph.in_degree(v) == 1:
             data = comp_graph.merge_edge(u, v)
             update_shortest_path_cost(comp_graph, u, device_topo, fast_link)
-        elif (comp_graph.out_degree(u) > 1 and comp_graph.get_shortest_path_cost(u) >=
+        elif comp_graph.out_degree(u) > 1 and (comp_graph.get_shortest_path_cost(u) - comp_graph.getOperatorCompCostByDevice(u,random_device) >=
               sum(comp_graph.get_shortest_path_cost(succ) for succ in comp_graph.successors(u))):
             data = comp_graph.merge_edge(u, v)
             update_shortest_path_cost(comp_graph, u, device_topo, fast_link)
